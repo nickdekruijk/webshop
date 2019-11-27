198515698b178877e8bd8f31e08bee4c7d9667a6
Nick de Kruijk <git@nickdekruijk.nl>
2019-11-27 18:47:04 +0100

    Added Webshop::showCart() (unfinished)

--- src/Webshop.php
+++ src/Webshop.php
@@ -24,4 +24,32 @@ class Webshop
         // Return the html
         return $html;
     }
+
+    // Return HTML table with the cart contents
+    public function showCart()
+    {
+        $html = '';
+        $html .= '<table class="webshop-cart">';
+        $html .= '<tr>';
+        $html .= '<td'. (config('webshop.product_columns.image') ? ' colspan="2"' : '') . '>Product</td>';
+        $html .= '<td>Price</td>';
+        $html .= '<td>Quantity</td>';
+        $html .= '<td>Total</td>';
+        $html .= '</tr>';
+        foreach (CartController::getItems() as $item) {
+            $html .= '<tr>';
+//             dd($item->product);
+            if (config('webshop.product_columns.image')) {
+                $html .= '<td><img src="' . $item->product[config('webshop.product_columns.image')] . '" alt=""></td>';
+            }
+            $html .= '<td><div class="webshop-cart-title">' . $item->product[config('webshop.product_columns.title')] . '</div><div class="webshop-cart-description">' . $item->product[config('webshop.product_columns.description')] . '</div></td>';
+            $html .= '<td class="webshop-cart-price">' . $item->product[config('webshop.product_columns.price')] . '</td>';
+            $html .= '<td class="webshop-cart-quantity">' . +$item->quantity . '</td>';
+            $html .= '<td class="webshop-cart-total">' . $item->quantity * $item->product[config('webshop.product_columns.price')] . '</td>';
+//             $html .= '<td>' . $item->product . '</td>';
+            $html .= '</tr>';
+        }
+        $html .= '</table>';
+        return $html;
+    }
 }
