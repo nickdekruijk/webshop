fac46971646e92cc53d7827f381f97a9dc8313cb
Nick de Kruijk <git@nickdekruijk.nl>
2019-11-27 16:03:57 +0100

    webshop-cart-show route

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,5 @@
 
 Route::group(['middleware' => ['web']], function () {
     Route::get(config('webshop.routes_prefix').'/cart/add/{product}/{quantity?}/{product_option?}', 'NickDeKruijk\Webshop\CartController@add')->name('webshop-cart-add');
+    Route::get(config('webshop.routes_prefix').'/cart', 'NickDeKruijk\Webshop\CartController@show')->name('webshop-cart-show');
 });
