94225ea6f355f5b24d63ec9877cc0aadee4b846e
Nick de Kruijk <git@nickdekruijk.nl>
2019-12-18 21:15:40 +0100

    PSR-2

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get(config('webshop.routes_prefix').'/cart/add/{product}/{quantity?}/{product_option?}', 'NickDeKruijk\Webshop\CartController@add')->name('webshop-cart-add');
+    Route::get(config('webshop.routes_prefix') . '/cart/add/{product}/{quantity?}/{product_option?}', 'NickDeKruijk\Webshop\CartController@add')->name('webshop-cart-add');
 });
