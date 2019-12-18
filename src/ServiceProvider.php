1b634bf626ba8e8818106bc3d807b109afa021bc
Nick de Kruijk <git@nickdekruijk.nl>
2019-12-18 21:14:18 +0100

    PSR-2

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,16 +11,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-//         $this->loadViewsFrom(__DIR__.'/views', 'webshop');
+        //         $this->loadViewsFrom(__DIR__.'/views', 'webshop');
         $this->publishes([
-            __DIR__.'/config.php' => config_path('webshop.php'),
+            __DIR__ . '/config.php' => config_path('webshop.php'),
         ], 'config');
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
         $this->loadTranslationsFrom(__DIR__ . '/lang', 'webshop');
         if (config('webshop.migrations', true)) {
-            $this->loadMigrationsFrom(__DIR__.'/migrations');
+            $this->loadMigrationsFrom(__DIR__ . '/migrations');
         }
-//         $this->loadRoutesFrom(__DIR__.'/routes.php');
+        //         $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
 
     /**
@@ -30,7 +30,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config.php', 'webshop');
+        $this->mergeConfigFrom(__DIR__ . '/config.php', 'webshop');
 
         // Register the main class to use with the facade
         $this->app->singleton('nickdekruijk-webshop', function () {
