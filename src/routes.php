3bc6bddd2dc11cc18e93ffe318799db47a2c9ad7
Nick de Kruijk <git@nickdekruijk.nl>
2019-12-18 21:16:04 +0100

    CartController@post route

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,5 @@
 
 Route::group(['middleware' => ['web']], function () {
     Route::get(config('webshop.routes_prefix') . '/cart/add/{product}/{quantity?}/{product_option?}', 'NickDeKruijk\Webshop\CartController@add')->name('webshop-cart-add');
+    Route::post(config('webshop.routes_prefix') . '/checkout', 'NickDeKruijk\Webshop\CartController@post')->name('webshop-checkout-post');
 });
