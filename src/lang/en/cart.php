51e9790dbf5b4c6d2f83b79de55350b5acc009d4
Nick de Kruijk <git@nickdekruijk.nl>
2019-12-18 21:13:29 +0100

    Localization

---
+++ src/lang/en/cart.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'price' => 'Price',
+    'quantity' => 'Quantity',
+    'total' => 'Total',
+    'product' => 'Product',
+    'dec_point' => '.',
+    'thousands_sep' => ',',
+    'subtotal' => 'Subtotal',
+    'weight' => 'Weight',
+    'total_to_pay' => 'Total amount to pay',
+    'no-shipping-possible' => 'No shipping possible',
+];
---
+++ src/lang/nl/cart.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'price' => 'Prijs',
+    'quantity' => 'Aantal',
+    'total' => 'Totaal',
+    'product' => 'Product',
+    'dec_point' => ',',
+    'thousands_sep' => '.',
+    'subtotal' => 'Subtotaal',
+    'weight' => 'Gewicht',
+    'total_to_pay' => 'Af te rekenen bedrag',
+    'no-shipping-possible' => 'Geen verzending mogelijk',
+];
