4763f814cd0c97b3ae3cef9a65a8b381900540b6
Nick de Kruijk <git@nickdekruijk.nl>
2020-06-26 11:25:39 +0200

    Remove cartIcon() method
    In your view use somthing like this instead:
    
    @if (Webshop::count())
        <span class="cart-count">{{ $count }}</span><i class="icon-basket"></i>
    @else
        '<i class="icon-basket cart-empty"></i>
    @endif

--- src/Webshop.php
+++ src/Webshop.php
@@ -11,25 +11,6 @@ use Session;
 
 class Webshop
 {
-    // Return html based on cart empty or not
-    // Example usage: {!! Webshop::cartIcon('<span class="cart-count">$count</span><i class="icon-basket"></i>','<i class="icon-basket cart-empty"></i>', true) !!}
-    public function cartIcon($html, $htmlEmpty = null, $unique = false)
-    {
-        // Get the amount of items in cart
-        $count = CartController::count($unique);
-
-        // If empty use $htmlEmpty if present
-        if ($count == 0 && $htmlEmpty) {
-            $html = $htmlEmpty;
-        }
-
-        // Replace $count string with actual count value
-        $html = str_replace('$count', $count, $html);
-
-        // Return the html
-        return $html;
-    }
-
     /**
      * Return total count of items in the cart.
      *
