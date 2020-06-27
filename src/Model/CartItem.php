91d76abcb22a41495c821c87abf7f380720f4fa9
Nick de Kruijk <git@nickdekruijk.nl>
2020-06-27 14:41:13 +0200

    Price attribute is now an object including VAT calculation results

--- src/Model/CartItem.php
+++ src/Model/CartItem.php
@@ -31,10 +31,24 @@ class CartItem extends Model
     public function getPriceAttribute($value)
     {
         if (config('webshop.product_option_model') && $this->option && config('webshop.product_option_columns.price')) {
-            return $this->option[config('webshop.product_option_columns.price')] ?: $this->product[config('webshop.product_columns.price')];
+            $price = ['price' => $this->option[config('webshop.product_option_columns.price')] ?: $this->product[config('webshop.product_columns.price')]];
         } else {
-            return $this->product[config('webshop.product_columns.price')];
+            $price = ['price' => $this->product[config('webshop.product_columns.price')]];
         }
+
+        $price['vat_included'] = $this->product->vat->included;
+        $price['vat_rate'] = $this->product->vat->rate;
+        if ($price['vat_included']) {
+            $price['price_including_vat'] = $price['price'];
+            $price['price_excluding_vat'] = number_format($price['price'] / ($price['vat_rate'] / 100 + 1), 2);
+            $price['price_vat'] = $price['price_including_vat'] - $price['price_excluding_vat'];
+        } else {
+            $price['price_including_vat'] = number_format($price['price'] * ($price['vat_rate'] / 100 + 1), 2);
+            $price['price_excluding_vat'] = $price['price'];
+            $price['price_vat'] = $price['price_including_vat'] - $price['price_excluding_vat'];
+        }
+
+        return (object) $price;
     }
 
     public function getWeightAttribute($value)
