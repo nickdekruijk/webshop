ce3500c6a8b6f7286c5526f9cd0e6462c4a9f1bf
Nick de Kruijk <git@nickdekruijk.nl>
2020-10-30 18:04:04 +0100

    Added Product options() relationship to example

--- src/examples/Product.php
+++ src/examples/Product.php
@@ -41,4 +41,9 @@ class Product extends Model
     {
         return $this->belongsTo("NickDeKruijk\Webshop\Model\Vat");
     }
+
+    public function options()
+    {
+        return $this->hasMany('App\Models\ProductOption');
+    }
 }
